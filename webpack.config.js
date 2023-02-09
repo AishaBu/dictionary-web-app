1bca0c2306ae9eff952f1b6e380e2eaec96e9d8c
CodedGlory <aisha.busari@gmail.com>
2023-02-09 17:43:36 -0500

    Add webpack and configure packages properly

--- webpack.config.js
+++ webpack.config.js
@@ -14,16 +14,39 @@ module.exports = {
     assetModuleFilename: "[name][ext]",
   },
 
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+
   module: {
     rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
 
       {
-        test: /\.html$/,
-        use: ["html-loader"],
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
       },
 
       {
