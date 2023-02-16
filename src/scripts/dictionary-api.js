e1e2e067b8d71b6f1bc9b832a7a86a6fc7843f09
CodedGlory <aisha.busari@gmail.com>
2023-02-16 09:34:34 -0500

    Make audio file play without overlapping

--- src/scripts/dictionary-api.js
+++ src/scripts/dictionary-api.js
@@ -6,14 +6,13 @@ import { noun } from "./global-variables";
 import { nounLine } from "./global-variables";
 import { playIcon } from "./global-variables";
 
-
 /*API CALL*/
 async function fetchVocabularyWords(searchFieldValue) {
   const apiKey = "585651c1-6ea4-4761-b3e7-d7af4eea540e";
   try {
     const response = await fetch(
       `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${searchFieldValue}?key=${apiKey}`
-    )
+    );
     if (!response.ok) {
       throw new Error(`HTTP error: ${response.status}`);
     }
@@ -45,39 +44,22 @@ function displayWords() {
 /*Create audio on click of icon*/
 function playAudio() {
   const promise = fetchVocabularyWords(searchField.value);
-  promise.then((data) =>
-    playIcon.addEventListener("click", () => {
-      const audio = new Audio(
-        `https://media.merriam-webster.com/audio/prons/en/us/mp3/${data[0].hwi.prs[0].sound.audio[0]}/${data[0].hwi.prs[0].sound.audio}.mp3`
-      );
-      audio.load();
-      audio.play(); 
-
-      audio.addEventListener('ended', ()=> {
-        location.reload();
-      })
-    })
-  );
+  promise.then((data) => {
+    const audio = new Audio(
+      `https://media.merriam-webster.com/audio/prons/en/us/mp3/${data[0].hwi.prs[0].sound.audio[0]}/${data[0].hwi.prs[0].sound.audio}.mp3`
+    );
+    audio.load();
+    audio.play();
+  });
 }
-
 
 /*Display dictionary words and play audio onsubmit*/
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   displayWords();
-/*Play audio on icon click*/
-playIcon.addEventListener("click", playAudio());
+
+  /*Play audio on icon click*/
+  playIcon.addEventListener("click", () => {
+    setInterval(playAudio(), 500);
+  });
 });
-
-//const promise = fetchVocabularyWords();
-//promise.then((data) => (noun.textContent = console.log (data[0])));
-//promise.then((data) => (console.log(data[0].hwi.prs[0].sound)));
-
-/*Console Log*
-    promise.then((data) => console.log((data[0].hwi.prs[0].sound.audio[0])));
-    promise.then((data) => console.log((data[0].hwi.prs[0].sound.audio)));
-    */
-
-//const audio = new Audio('https://media.merriam-webster.com/audio/prons/en/us/mp3/p/pajama02.mp3');
-//audio.play();
-//new Audio(`https://media.merriam-webster.com/audio/prons/en/us/mp3/${(data[0].hwi.prs[0].sound.audio[0])}/${(data[0].hwi.prs[0].sound.audio)}.mp3`)
